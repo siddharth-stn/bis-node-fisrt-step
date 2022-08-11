b9b73b4b523845fbc20e9acc14f5e132eb3a55c1
Siddharth Pande <sidarjunstn.sp@gmail.com>
2022-08-11 19:46:05 +0530

    first node project completed

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 require('dotenv').config();
 
 const http = require('http');
+const fs = require('fs');
+const { dirname } = require('path');
 
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
 
 const server = http.createServer((req, res) => {
-    console.log(req.URL);
+    console.log(req.url);
+    console.log();
+
+    if (req.url === '/index') {
+        fs.readFile(__dirname + req.url + '.html', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.end(data);
+        });
+    } else if (req.url === '/contact-me') {
+        fs.readFile(__dirname + req.url + '.html', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.end(data);
+        });
+    } else if (req.url === '/about') {
+        fs.readFile(__dirname + req.url + '.html', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.end(data);
+        });
+    } else {
+        fs.readFile(__dirname + '/404.html', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.end(data);
+        });
+    } 
 });
 
 server.listen(port, hostname, () => {
